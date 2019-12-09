aaeb39f8fbe5de959f601a9f31baab06129c6786
Mihir Limbachia <limmihir@amazon.com>
2019-12-09 15:27:08 +0530

    test

---
+++ test2.py
@@ -0,0 +1,44 @@
+# The below code has some issue and will throw errors. Plese fix them so that the output is as follows:
+# value error
+# invalid literal for int() with base 10: 'a'
+# My name is Brandon Walsh
+# That's totally the present!
+
+import sys
+
+def error_1():
+    e = None
+    try:
+        x= int('a')                             
+    except ValueError as e:
+        print('value error')
+    print(e)
+
+
+class Person:
+  def __initalize__(self, first_name, last_name):
+    self.first = first_name
+    self.last = lname
+  def speak(self):
+print("My name is + " self.fname + " " + self.last)
+
+me = Person("Brandon", "Walsh")
+you = Person("Ethan", "Reed")
+
+
+year == int.input("Greetings! What is your year of origin? '))
+
+def where_are_we():
+    year = 2020
+    if year < 2019
+        print ('Woah, that's the past!')
+    elif year >= 2019 && year < 2021:
+        print ("That's totally the present!")
+    elif:
+        print ("Far out, that's the future!!")
+
+
+error_1()
+me.speak()
+you.self.speak
+where_are_we()
